41fc9587e87bea38ee4c52f0f7df1173c7d0fc56
charliebkw <charlotte.birkinshaw@bigpond.com>
2023-09-06 16:24:54 +1000

    Register Models to the database

--- django_project/django_app/admin.py
+++ django_project/django_app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Community, Project, Venue, InterestType, Event, News, Account, AccountInterest, DiscussionPost, TicketTierType, Ticket
 
 # Register your models here.
+admin.site.register(Community)
+admin.site.register(Project)
+admin.site.register(Venue)
+admin.site.register(Event)
+admin.site.register(InterestType)
+admin.site.register(Account)
+admin.site.register(News)
+admin.site.register(AccountInterest)
+admin.site.register(DiscussionPost)
+admin.site.register(TicketTierType)
+admin.site.register(Ticket)
