013876d6c49ab3f6a4d827aac172ac0bad4d904c
charliebkw <charlotte.birkinshaw@bigpond.com>
2023-10-04 14:18:56 +1000

    Updates to the database tables

--- django_project/django_app/models.py
+++ django_project/django_app/models.py
@@ -18,21 +18,14 @@ class Project(models.Model):
 
 class Venue(models.Model):
     locationName = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
     capacity = models.IntegerField()
     gpsLongitude = models.FloatField()
     gpsLatitude = models.FloatField()
     
 class InterestType(models.Model):
-    InterestType = models.CharField(max_length=100)
+    interestType = models.CharField(max_length=100)
 
-class Event(models.Model):
-    project = models.ForeignKey(Community, on_delete=models.CASCADE)
-    interestType = models.ForeignKey(InterestType, on_delete=models.CASCADE)
-    venue = models.ForeignKey(Venue, on_delete=models.CASCADE)
-    startDateTime = models.DateTimeField(default=datetime.now)
-    duration = models.IntegerField()
-    price = models.FloatField()
-    name = models.CharField(max_length=100)
 
 class Account(models.Model):
     community = models.ForeignKey(Community, on_delete=models.CASCADE)
@@ -43,6 +36,18 @@ class Account(models.Model):
     email = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
     salt = models.CharField(max_length=100)
+    token = models.CharField(max_length=200)
+
+class Event(models.Model):
+    project = models.ForeignKey(Community, on_delete=models.CASCADE)
+    interestType = models.ForeignKey(InterestType, on_delete=models.CASCADE)
+    venue = models.ForeignKey(Venue, on_delete=models.CASCADE)
+    startDateTime = models.DateTimeField(default=datetime.now)
+    duration = models.IntegerField()
+    price = models.FloatField()
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=400)
+    creator = models.ForeignKey(Account, on_delete=models.CASCADE)
 
 class News(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
