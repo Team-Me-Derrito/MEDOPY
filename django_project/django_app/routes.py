f39ea25add16062b9300aee3c6259c32f6b91625
charliebkw <charlotte.birkinshaw@bigpond.com>
2023-10-19 20:10:06 +1000

    new comments added

--- django_project/django_app/routes.py
+++ django_project/django_app/routes.py
@@ -1,14 +1,18 @@
+"""
+routes.py
+    This file includes the functions that are called when someone calls a particular url from the urls.py file. It will often call the queries.py file for database interactions that are repeated often.
+"""
 from .models import Event, Venue, Project, Account, AccountInterest, Ticket, Community, DiscussionPost, InterestType
-from django.http import JsonResponse, HttpResponse
+from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from datetime import datetime, timedelta
+from datetime import datetime
 import json, re
 from . import queries, security
 
 """
 getInterestEvents()
     Gets all events that match the current account's community and interests.
-    Required request data: token + account_id.
+    request: data["account_id", "token"]
 """
 @csrf_exempt
 def getInterestEvents(request):
@@ -17,13 +21,18 @@ def getInterestEvents(request):
         data = json.loads(data)
         print("Getting interest events")
 
-        events = queries.getInterestEventsByAccount(data["Token"], data["account_id"])
-        return JsonResponse({"events": events})
+        query = queries.getAccount(data["account_id"], data["Token"])
+        if not query["success"]:
+            return JsonResponse(query)
+        
+        events = queries.getInterestEventsByAccount(query["account"])
+        return JsonResponse({"events": events, "success": True})
+
 
 """
 getEventInfo()
     Gets all necessary event info from the id for the event page.
-    Request data: event_id
+    request: data["event_id"]
 """
 @csrf_exempt    
 def getEventInfo(request):
@@ -45,10 +54,12 @@ def getEventInfo(request):
             "creatorName": event.creator.accountName,
             "creatorEmail": event.creator.email,
             "creatorPhoneNumber": event.creator.phoneNumber,
-            "creator_id": event.creator.pk
+            "creator_id": event.creator.pk,
+            "success": True
             }
 
         return JsonResponse(struct)
+
 
 """
 createAccount()
@@ -84,20 +95,25 @@ def createAccount(request):
             accountInterest.save()
 
         return JsonResponse({"account_id": newAccount.pk, "token": token, "success": True})
+
+
 """
 getAllEvents()
     Returns all the events in the database
+    request:
 """
 @csrf_exempt
 def getAllEvents(request):
     events = []
     for event in Event.objects.all():
         events.append({"eventID": event.id, "eventName": event.name, "description": event.description, "venue": event.venue.locationName})
-    return JsonResponse({"events": events})
+    return JsonResponse({"events": events, "success": True})
+
 
 """
 getAllCommuntiyEvents()
     Returns all the events of an account's community.
+    request: data["account_id", "Token"]
 """
 @csrf_exempt
 def getAllCommunityEvents(request):
@@ -105,16 +121,18 @@ def getAllCommunityEvents(request):
         data = request.body.decode("utf-8")
         data = json.loads(data)
 
-        account = Account.objects.get(pk=data["account_id"], token=data["Token"])
-        community = account.community
-        
-        events = queries.getEventsByCommunty(community.id)
+        query = queries.getAccount(data["account_id"], data["Token"])
+        if not query["success"]:
+            return JsonResponse(query)
 
-        return JsonResponse({"events": events})
+        community = query["account"].community
+        events = queries.getEventsByCommunty(community.pk)
+
+        return JsonResponse({"events": events, "success": True})
 
 """
 getAllCommunityEventsDisplay()
-    Request: community_id
+    Request: data["community_id"]
 """ 
 @csrf_exempt   
 def getAllCommunityEventsDisplay(request):
@@ -123,7 +141,7 @@ def getAllCommunityEventsDisplay(request):
         data = json.loads(data)
 
         events = queries.getEventsByCommunty(data["community_id"])
-        return JsonResponse({"events": events})
+        return JsonResponse({"events": events, "success": True})
     
 """
 getUpcommingEvents()
@@ -135,14 +153,19 @@ def getUpcommingEvents(request):
         data = request.body.decode("utf-8")
         data = json.loads(data)
 
-        account = Account.objects.get(pk=data["account_id"], token=data["Token"])
-        community_id = account.community.id
+        query = queries.getAccount(data["account_id"], data["Token"])
+        if not query["success"]:
+            return JsonResponse(query)
+
+        community_id = query["account"].community.pk
         events = queries.getNextMonthCommunityEvents(community_id)
-        return JsonResponse({"events": events})
-    
+        return JsonResponse({"events": events, "success": True})
+
+
 """
 getTicketed()
     Gets all event ids of an accounts upcomming ticketed events.
+    request: data["account_id", "Token"]
 """
 @csrf_exempt
 def getTicketed(request):
@@ -150,14 +173,17 @@ def getTicketed(request):
         data = request.body.decode("utf-8")
         data = json.loads(data)
 
-        account = Account.objects.get(pk=data["account_id"], token=data["token"])
-        events = queries.getTicketedEvents(account)
+        query = queries.getAccount(data["account_id"], data["Token"])
+        if not query["success"]:
+            return JsonResponse(query)
 
-        return JsonResponse({"events": events})
+        events = queries.getTicketedEvents(query["account"])
+
+        return JsonResponse({"events": events, "success": True})
     
 """
 getDiscussionPosts()
-    Gets all the discussion posts for the users community
+    Gets all the discussion posts for the current user's community
 """
 @csrf_exempt
 def getDiscussionPosts(request):
@@ -165,8 +191,12 @@ def getDiscussionPosts(request):
         data = request.body.decode("utf-8")
         data = json.loads(data)
         print("communits posts data is", data)
-        account = Account.objects.get(pk=data["account_id"], token=data["Token"])
-        community = account.community
+        
+        query = queries.getAccount(data["account_id"], data["Token"])
+        if not query["success"]:
+            return JsonResponse(query)
+        
+        community = query["account"].community
 
         posts = []
         for post in DiscussionPost.objects.filter(community=community):
@@ -174,14 +204,13 @@ def getDiscussionPosts(request):
                 "accountName": post.account.accountName, 
                 "timestamp":post.timestamp, 
                 "text":post.text})
-        # for post in DiscussionPost.objects.all():
-        #     posts.append({"accountName": post.account.accountName, "timestamp":post.timestamp, "text":post.text})
-
-        return JsonResponse({"posts": posts})
+        
+        return JsonResponse({"posts": posts, "success": True})
     
 """
 newDiscussionPost()
-    User can create a new post one the discussion board
+    User can create a new post for the discussion board.
+    request: data["account_id", "Token", "post_text"]
 """
 @csrf_exempt
 def newDiscussionPost(request):
@@ -189,25 +218,38 @@ def newDiscussionPost(request):
         data = request.body.decode("utf-8")
         data = json.loads(data)
 
-        account = Account.objects.get(pk=data["account_id"], token=data["token"])
-        community = account.community
+        query = queries.getAccount(data["account_id"], data["Token"])
+        if not query["success"]:
+            return JsonResponse(query)
+
+        community = query["account"].community
 
         post = DiscussionPost(
             community=community,
-            account=account,
+            account=query["account"],
             timestamp=datetime.now(),
             text=data["post_text"]
             )
         post.save()
+        return JsonResponse({"success": True, "message": "New post made"})
 
+
+"""
+searchEvents()
+    For searching functions when using search bar to get event by name.
+    request: data["account_id", "Token", "query"]
+"""
 @csrf_exempt  
 def searchEvents(request):
     if request.method == "POST":
         data = request.body.decode("utf-8")
         data = json.loads(data)
 
-        account = Account.objects.get(pk=data["account_id"], token=data["Token"])
-        community = account.community
+        query = queries.getAccount(data["account_id"], data["Token"])
+        if not query["success"]:
+            return JsonResponse(query)
+
+        community = query["account"].community
         projects = Project.objects.filter(community=community)
 
         events = []
@@ -215,42 +257,12 @@ def searchEvents(request):
             if (event.project in projects) and (re.search(data["query"], event.name)):
                 events.append({"id": event.id, "name": event.name})
 
-        return JsonResponse({"events": events})
+        return JsonResponse({"events": events, "success": True})
 
-"""
-Get the 20 closest events to the given current_gps.
-"""
-@csrf_exempt
-def getCloseEvents(current_gps):
-    events = Event.objects.all()
-    event_venues = Venue.objects.all()
-    #filter by venues connected to events
-    
-    gps_distances = []
-    for venue in event_venues:
-        gps_distances.append((distanceBetween({"longitude":venue.longitude, "latitude":venue.latitue}, current_gps), venue))
-    gps_distances = sorted(gps_distances)
-    if gps_distances >= 20:
-        gps_distances = gps_distances[0:20]
-
-    topResults = {}
-    for i, venue in enumerate(gps_distances):
-        event = Event.objects.filter(venue=venue)
-        topResults[i] = {"name": event.name, "startDate": event.startDateTime, "price": event.price, "interestType": event.interestType.interestType}
-    return JsonResponse(topResults)
-    
-
-"""
-Function to find distance between 2 gps objects.
-"""
-@csrf_exempt
-def distanceBetween(gps1, gps2):
-    longitude = gps1["longitude"] - gps2["longitude"]
-    latitude = gps1["latitude"] - gps2["latitude"]
-    return ((longitude ** 2 + latitude ** 2) ** 0.5)
 
 """
 getCommunityInfo()
+    Get info for a particular community required for the display.
     request: data["community_id]
 """
 @csrf_exempt
@@ -328,14 +340,17 @@ def getCommunityInfo(request):
 
         community_info = {
             "community_name": community.communityName, 
-            "community_id": community.pk, 
-            "scores": scores
+            "community_id": community.pk,
+            "scores": scores,
+            "success": True
             }
         return JsonResponse(community_info)
     
 
 """
 getProjectsInCommunity()
+    Gets all of the projects in a particular community and sends their
+    info back in a json structure.
     request: data["community_id"]
 """
 @csrf_exempt
@@ -362,6 +377,9 @@ def getProjectsInCommunity(request):
     
 """
 getCommunityInterests()
+    Gets all the interests in a particular community and lists them
+    in a json structure with the amount of people with that interest in
+    the community.
     request: data["community_id"]
 """
 @csrf_exempt
@@ -384,8 +402,8 @@ def getCommunityInterests(request):
     
 """
 getCommunityPosts()
+    Returns the posts on a community's discussion board
     request: data["community_id"]
-    returns the last 50 posts
 """
 @csrf_exempt
 def getCommunityPosts(request):
@@ -408,6 +426,14 @@ def getCommunityPosts(request):
     
 """
 login()
+    Function for accounts to login. The email and password the user has entered
+    is received and it is checked if this email exists in the system if so the 
+    salt is retrieved and added to the inserted password. The hashed password is
+    then check with the existing accounts password in the db.
+    If the password and email match a new session token is generated and stored
+    under this account in the db and a success response is sent back to the app
+    with the account id and session token to be stored and sent back when a new
+    request is made.
     request: data["email", "password"]
 
     return:
@@ -425,20 +451,21 @@ def login(request):
 
         salt = queries.getSalt(data["Email"])
         if salt is not None:
-            password_hashed = security.hash(data["Password"], queries.getSalt(data["Email"]))
+            password_hashed = security.hash(data["Password"], salt)
             response = queries.verify(data["Email"], password_hashed)
         else:
-            return JsonResponse({"success": False})
+            return JsonResponse({"success": False, "message": "This email doesn't exist in the database"})
 
         if response is not None:
             login = {"success": True, "account_id": response["account_id"], "token": response["token"]}
         else:
-            login = {"success": False}
+            login = {"success": False, "message": "Account's email and password couldn't be matched"}
     return JsonResponse(login)
 
 
 """
 createPost()
+    Allows accounts to create a new post on the communtiy discussion board
     request: data["account_id", "token", "message"]
 """
 @csrf_exempt
@@ -451,10 +478,9 @@ def createPost(request):
         return JsonResponse(result)
 
 
-
-# get account info
 """
 getAccountInfo()
+    Uses the account id and token to get all displayable information about the account.
     request: data["account_id", "token"]
 """
 @csrf_exempt
@@ -464,6 +490,10 @@ def getAccountInfo(request):
         data = json.loads(data)
 
         print(data)
+        query = queries.getAccount(data["account_id"], data["Token"])
+        if not query["success"]:
+            return JsonResponse(query)
+        
         result = queries.getAccountInfo(data["account_id"], data["Token"])
         
         return JsonResponse(result)
@@ -471,7 +501,8 @@ def getAccountInfo(request):
     
 """
 getCommunities()
-    request:
+    Gets all of the communities
+    request: data[]
 """
 @csrf_exempt
 def getCommunities(request):
@@ -483,7 +514,8 @@ def getCommunities(request):
 
 """
 createEvent()
-request: data[""]
+    Creates a new event in the database.
+    request: data["project_id", "interest_id", "venue_id", "startDateTime", "duration", "price", "name", "description", "account_id", "Token"]
 """
 @csrf_exempt
 def createEvent(request):
@@ -491,8 +523,6 @@ def createEvent(request):
         data = request.body.decode("utf-8")
         data = json.loads(data)
         print("create event data ", data)
-        #project_id, interestType_id, venue_id, startDateTime, duration, price, name, description, account_id, token
-#{'Token': 1, 'account_id': 1, 'name': 'The ', 'description': 'F', 'price': 0, 'duration': 4, 'startDateTime': '2023-10-17T13:49:21.950Z', 'venue_id': 1, 'project_id': 1, 'interest_id': 1}
         result = queries.createEvent(
             data["project_id"], data["interest_id"], data["venue_id"], data["startDateTime"], data["duration"], data["price"], data["name"], data["description"], data["account_id"], data["Token"]
         )
@@ -501,7 +531,8 @@ def createEvent(request):
 
 """
  getVenues()
-    request:
+    Gets all of the venues in the database.
+    request: data[]
  """
 @csrf_exempt
 def getVenues(request):
@@ -523,6 +554,8 @@ def getVenues(request):
     
 """
 getInterestTypes()
+    Gets all of the interest types in the database.
+    request: data[]
 """
 @csrf_exempt
 def getInterestTypes(request):
@@ -541,6 +574,8 @@ def getInterestTypes(request):
     
 """
 getProjects()
+    Gets all of the projects in the database.
+    request: data[]
 """
 @csrf_exempt
 def getProjects(request):
@@ -562,7 +597,8 @@ def getProjects(request):
 
 """
 getAttendance()
-    request: data["event_id]
+    Gets whether the current user is attending an event or not.
+    request: data["event_id", "Token", "account_id"]
 """
 @csrf_exempt
 def getAttendance(request):
@@ -581,8 +617,9 @@ def getAttendance(request):
 
 
 """
-getAttendance()
-    request: data["event_id]
+setAttendance()
+    Sets the attendance of an event as specified in the request data.
+    request: data["event_id", "account_id", "Token", "attendance"]
 """
 @csrf_exempt
 def setAttendance(request):
@@ -597,6 +634,7 @@ def setAttendance(request):
     
 """
 deleteEvent()
+    Removes an event if the account sending the request is the creator of the event.
     request: data["event_id", "account_id", "Token"]
 """
 @csrf_exempt
@@ -615,6 +653,7 @@ def deleteEvent(request):
         
 """
 getUserScore()
+    Calculates the current user's score based off of their community engagement.
     request: data["account_id", "Token"]
 """
 @csrf_exempt
@@ -632,6 +671,7 @@ def getUserScore(request):
         
 """
 getCommunityScore()
+    Calculates the score of a given community based of its members' engagement.
     request: data["accout_id", "token"]
 """
 @csrf_exempt
@@ -646,8 +686,8 @@ def getCommunityScore(request):
 
 """
 updateAccount()
-    For updating an existing acccount
-    request: account_id, Token, accountName, birthday, gender, phoneNumber
+    For updating an existing acccount's information
+    request: data["account_id", "Token", "accountName", "birthday", "gender", "phoneNumber"]
 """
 @csrf_exempt
 def updateAccount(request):
